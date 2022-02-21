1ce1af4ffb813adf5a112bf819c26cd1e485fd3b
Osman Mohammad <usman.rahmati61@gmail.com>
2022-02-20 18:08:07 -0800

    3

--- quiz3/quiz3.cpp
+++ quiz3/quiz3.cpp
@@ -8,6 +8,13 @@ int main()
     unsigned seed;
     unsigned seed = time(0);
     int num1,num2,num3;
-    
+    num1 = rand();
+    num2 = rand();
+    num3 = rand();
+    cout << "The first number is "<< num1 << endl;
+    cout << "The second number is "<< num2 << endl;
+    cout << "The third number is "<< num3 << endl;
+    cout << "The sum of the numbers is "<< num1 + num2 + num3<<endl;
+    cout << "The average of the numbers is " << (num1 +num2 + num3)/3;
 
 }
