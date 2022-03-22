cfec36901f9a78270f938c0c1073e6c9cc4bdb4a
Osman Mohammad <usman.rahmati61@gmail.com>
2022-03-22 16:29:00 -0700

    progress

--- quiz5.cpp
+++ quiz5.cpp
@@ -5,8 +5,18 @@
 #include <ctime>
 using namespace std;
 int main(){
+    double rdnum;
+    int minValue = 0;
+    int maxValue = 99;
+
+
     srand(time(0));
-    rdnum = rand( ) ;
-    count << rdum;
+    int count;
+    for (count = 0; count < 5; count++)
+    {
+        rdnum =(rand()% (maxValue - minValue + 1)) + minValue;
+        cout << rdnum << "\n" << endl;
+    }
+        
 
 }
