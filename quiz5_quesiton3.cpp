824c1f53e3b95e0aac791fbff9e93eea6b956b70
Osman Mohammad <usman.rahmati61@gmail.com>
2022-03-22 21:10:33 -0700

    added input validaiton

--- quiz5_quesiton3.cpp
+++ quiz5_quesiton3.cpp
@@ -3,8 +3,21 @@
 using namespace std;
 int main()
 {
-    int N = 1;
-    int M = 25;
+    int N ;
+    int M ;
+    cout << "type the beginning number";
+    cin >> N;
+    cout << "type the end number";
+    cin >> M;
+    while ( N < 1 && M > 99)
+    {
+        cout << "The numbers must range 1 to 99 ";
+        cout << "type the beginning number";
+        cin >> N;
+        cout << "type the end number";
+        cin >> M;
+    }
+
     int num;
     cout << "The prime numbers from "<< N << " to "<< M <<" are: ";
     for (num = N; num <= M; num++)
