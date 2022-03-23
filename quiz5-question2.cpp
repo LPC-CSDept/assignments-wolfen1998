91d723dfaf49fd77c1dddff4fa0efd2f462759ab
Osman Mohammad <usman.rahmati61@gmail.com>
2022-03-22 17:06:19 -0700

    no idea what I am doing

---
+++ quiz5-question2.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <iomanip>
+#include <math.h>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+int main()
+{
+    int N = 5 ;
+    int M = 6 ;
+    int num = 2;
+    N *= N;
+    cout << N << endl;
+
+   
+
+}
