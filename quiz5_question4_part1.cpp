6f3a917041991afaeef7eccb02f46b501a0a7d1d
Osman Mohammad <usman.rahmati61@gmail.com>
2022-03-22 21:12:41 -0700

    added the for loop

--- quiz5_question4_part1.cpp
+++ quiz5_question4_part1.cpp
@@ -3,6 +3,7 @@
 #include <string.h>
 #include <ctype.h>
 using namespace std;
+
 int main()
 {
 	int num, emp_num;
@@ -11,3 +12,34 @@ int main()
     ofstream outputFile;
     outputFile.open("Employee.txt");
 	for (num = 1; num <= emp_num; num++)
+	{
+		
+		
+
+		cout << "Enter the Employee ID ";
+		int ID;
+		cin >> ID;
+		outputFile << ID << endl;
+		cin.ignore();
+		cout << "Enter the Employee Name ";
+		string name;
+    	getline (cin,name);
+		outputFile << name << endl;
+
+		cout << "Enter the Employee's Department ";
+		string emp_dep;
+		cin >> emp_dep;
+		outputFile << emp_dep << endl;
+
+		cout << "Enter the Employee's salary ";
+		double emp_salary;
+		cin >> emp_salary;
+		outputFile << emp_salary << endl;
+
+		
+	}
+    
+    
+    
+
+}
