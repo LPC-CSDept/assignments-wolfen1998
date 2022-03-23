d884d6337200fcd699c8bce162e0277917bdb519
Osman Mohammad <usman.rahmati61@gmail.com>
2022-03-22 20:57:01 -0700

    rest of the code

--- quiz5_question2.cpp
+++ quiz5_question2.cpp
@@ -3,4 +3,21 @@ using namespace std;
 int main(){
     int N;
     int M;
+    cout << "Enter Your Base Number: ";
+    cin >> N;
+    cout << "Enter Your Exponent: ";
+    cin >> M;
+    cout << N << " to power numbers are" << endl;
+    int num ;
+    int outputs = 1;
+    for (num = 1; num <= M; num++ )
+    {
+        outputs *= N;
+        cout <<outputs << endl;
+    }
+    
+
+   
+
+}
 }
