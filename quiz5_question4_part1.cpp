4276790001e802457cc563dbe51541391215f32c
Osman Mohammad <usman.rahmati61@gmail.com>
2022-03-22 21:12:15 -0700

    progress

--- quiz5_question4_part1.cpp
+++ quiz5_question4_part1.cpp
@@ -3,3 +3,11 @@
 #include <string.h>
 #include <ctype.h>
 using namespace std;
+int main()
+{
+	int num, emp_num;
+	cout << "How many employees? ";
+	cin >> emp_num;
+    ofstream outputFile;
+    outputFile.open("Employee.txt");
+	for (num = 1; num <= emp_num; num++)
