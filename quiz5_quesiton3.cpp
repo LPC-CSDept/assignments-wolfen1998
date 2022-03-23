3c84879684ca910d05c675d86470337c743db952
Osman Mohammad <usman.rahmati61@gmail.com>
2022-03-22 20:59:57 -0700

    logic done

--- quiz5_quesiton3.cpp
+++ quiz5_quesiton3.cpp
@@ -6,3 +6,24 @@ int main()
     int N = 1;
     int M = 25;
     int num;
+    cout << "The prime numbers from "<< N << " to "<< M <<" are: ";
+    for (num = N; num <= M; num++)
+    {
+        if (M == 2)
+        {
+            cout << "2 is a prime number.";
+            break;
+        }
+        else if (num==2)
+        {
+            cout << num << ", ";
+        }
+        else if (num % 2 != 0)
+            cout << num<< ", ";
+    
+    }
+    
+
+   
+
+}
