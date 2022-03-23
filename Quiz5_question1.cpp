6d22932886faca1acf51a86c998f4652149d3b4b
Osman Mohammad <usman.rahmati61@gmail.com>
2022-03-22 20:52:14 -0700

    inttialize variables

--- Quiz5_question1.cpp
+++ Quiz5_question1.cpp
@@ -5,4 +5,11 @@
 #include <ctime>
 using namespace std;
 int main(){
+    double rdnum;
+    int minValue = 0;
+    int maxValue = 99;
+    srand(time(0));
+    int rdnum1,rdnum2,rdnum3,rdnum4,rdnum5;
+
+    
 }
