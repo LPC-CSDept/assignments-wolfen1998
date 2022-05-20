6cc93beb07e0ef43fe3526e0f3da1c2af0737027
wolfen1998 <99054525+wolfen1998@users.noreply.github.com>
2022-05-20 02:22:49 +0000

    finished the main function

--- Quiz 6 -1.cpp
+++ Quiz 6 -1.cpp
@@ -6,5 +6,18 @@ void swapTwoValues(int &, int &);
 void swapTwoValues(float &, float &);
 
 int main(){
+  double Fvalue1, Fvalue2l
+  int Ivalue, Ivaluel;
+  cout << "Enter Two integer values (with space in between the numbers) to be swapped ";
+  cin >> Ivalue1 >> Ivalue2;
+  cout << << "Enter Two floating values (with space in between the numbers) to be swapped ";
+  cin >> Fvalue1 >> Fvalue2;
+ swapTwoValues(Ivalue1 , Ivalue2) ;
+ cout << Ivalue1 << " " << Ivalue2 << endl;
+ cout << Fvalue1 << " " << Fvalue2 << endl;
+}
+
+
+
   
 }
