9129a595955bd5df24f9f01ef85aa670fd8de080
wolfen1998 <99054525+wolfen1998@users.noreply.github.com>
2022-05-20 02:57:25 +0000

    all 3 functions

--- Quiz 6 - 3.cpp
+++ Quiz 6 - 3.cpp
@@ -9,9 +9,44 @@ void gerRandnum(int &, int &, int &);
 int  findMin(int , int , int ); 
 void printResult(int , int , int , int );
 
+
 int main(){
-  int n1,n2,n3, min;
+    int n1,n2,n3, min;
     gerRandnum(n1, n2, n3);
     min = findMin(n1,n2,n3);
     printResult(n1 , n2 , n3 , min );
+    
+    
 }
+void gerRandnum(int &n1, int &n2, int &n3){
+    srand(time(0));
+    n1 =rand();
+    n2 = rand();
+    n3 = rand();
+}
+int  findMin(int n1, int n2, int n3){
+if (n1 < n2 && n1 < n3)
+{
+    
+    return n1;
+}
+else if (n2 < n1 && n2 < n3)
+{
+    
+    return n2;
+}
+else if (n3 < n1 && n3 < n2)
+{
+    
+    return n3;
+}
+
+}
+void printResult(int n1, int n2, int n3, int min){
+    cout << n1<< " " << n2<< " " <<" " << n3 << endl;
+    
+    cout  << "The lowest number is:  " << min ;
+}
+
+
+
