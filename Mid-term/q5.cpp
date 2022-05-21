4d8e31fc770bf723036fb324522fc3812df7f2da
wolfen1998 <99054525+wolfen1998@users.noreply.github.com>
2022-05-21 08:09:53 +0000

    writing the main function

--- Mid-term/q5.cpp
+++ Mid-term/q5.cpp
@@ -1,18 +1,47 @@
 #include <iostream>
 using namespace std;
 
-int getinput(int, int);
-bool isSamme(int,int);
+void getInput(int &, int &);
+bool isSame(int,int);
 void intDivision(int,int);
 void fileWrite(int,int);
 
 
 int main(){
+  bool check;
   int n1,n2;
-  getinput(n1,n2);
-
-
-
+  getInput(n1,n2);
+  cout << "The two numbers are: "<< n1 << " " << n2 << endl;
+  check = isSame(n1,n2);
+  if (check == true)
+  {
+    cout << "The numbers are the same." << endl;
+    exit(0);
+  }
+  else
+  {
+    check == false;
+  }
 
   
+  
+}
+
+void getInput(int &n1,int &n2){
+  cout << "Type two integer numbers with space in between them: ";
+  cin >> n1 >> n2;
+  
+
+
+
+}
+bool isSame(int n1, int n2)
+{
+  if (n1 == n2){
+    return true;
+  }
+  else
+  {
+    return false;
+  }
 }
