8d93a32490bea9b492bd356a9ff0d130a55e9d6d
wolfen1998 <99054525+wolfen1998@users.noreply.github.com>
2022-05-21 08:45:37 +0000

    trying to make the code repeat if the user types y

--- Mid-term/q5.cpp
+++ Mid-term/q5.cpp
@@ -6,6 +6,7 @@ void getInput(int &, int &);
 bool isSame(int,int);
 void intDivision(int &, int &, int &);
 void fileWrite(int,int);
+void repeat();
 
 
 int main(){
@@ -27,13 +28,14 @@ int main(){
   ofstream writeToFile;
   writeToFile.open("numbers.txt");
   writeToFile << result;
-  cout << "type y to repeat the program. Type anything else to quit" << endl;
+  cout << "Type y to repeat the program. Type anything else to quit" << endl;
   string repeatOrNot;
   cin >> repeatOrNot;
+
   
   while (repeatOrNot != "y")
     {
-    main();
+    repeat();
     }
   
 }
@@ -65,4 +67,35 @@ void intDivision(int &n1, int &n2, int &result){
   {
     result = n2/n1;
   }
+}
+
+void repeat(){
+  bool check;
+  int result;
+  
+  int n1,n2;
+  getInput(n1,n2);
+  cout << "The two numbers are: "<< n1 << " " << n2 << endl;
+  check = isSame(n1,n2);
+  if (check == true)
+  {
+    cout << "The numbers are the same." << endl;
+    exit(0);
+  }
+  intDivision(n1,n2,result);
+  cout<< "The integer of the two given numbers is: " << result << endl;
+  cout << "The number will be written to text file: numbers.txt" << endl;
+  ofstream writeToFile;
+  writeToFile.open("numbers.txt");
+  writeToFile << result;
+  cout << "Type y to repeat the program. Type anything else to quit" << endl;
+  string repeatOrNot;
+  cin >> repeatOrNot;
+
+  
+  while (repeatOrNot != "y")
+    {
+    repeat();;
+    }
+  
 }
